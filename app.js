8c39262f565bca9553cceb305515c9a935edff4c
cup <2078048816@qq.com>
2024-04-19 00:05:00 +0800

    feat: 删除无用代码

--- app.js
+++ app.js
@@ -29,40 +29,5 @@ app.listen(config.port, () => {
     console.log(`listening on ${config.port}`);
 })
 
-
-const { Server } = require('socket.io');
-const chatList = [];
-const server = http.createServer(app.callback());
-const io = new Server(server, {
-    serveClient: false,
-    cors: {
-        origin: '*', // from the screenshot you provided
-        methods: ['GET', 'POST'],
-    },
-});
-
-const chatModel = require("./server/model/Chat")
-io.on('connection', (socket) => {
-    // 传进来的socket变量为当前本次链接，而io是全局链接
-    socket.emit('fresh-message', chatList);
-    socket.on('send-message', (user_id, toUser_id, message, name) => {
-        chatModel.create({
-            user_id,
-            toUser_id,
-            message,
-            name
-        })
-        chatList.push({
-            name,
-            user_id,
-            message,
-        });
-        console.log(chatList)
-        io.emit('fresh-message', chatList);
-    });
-});
-io.listen(3002);
-
-
 const importGlobal = require("./utils")
 importGlobal()
