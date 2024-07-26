16d534e427f8699f711ae758f8a11983564d3489
cup <2078048816@qq.com>
2024-07-26 16:28:19 +0800

    feat: user pdf

--- server/controller/history.js
+++ server/controller/history.js
@@ -60,4 +60,49 @@ router.post('/getAllEvaluateHistory', async ctx => {
   ctx.suc("查询成功", allHistory);
 });
 
+router.post('/getPersonalEvaluateList', async ctx => {
+  const data = ctx.request.body;
+  const filteredObj = Object.fromEntries(
+    Object.entries(data).filter(([key, value]) => value !== '')
+  );
+
+  // 构建过滤条件
+  let where = {
+    user_id: data.userId
+  };
+  if (filteredObj.department) {
+    where.department = filteredObj.department;
+  }
+  if (filteredObj.position) {
+    where.position = filteredObj.position;
+  }
+
+  // 使用过滤条件查询
+  const [err, allHistory] = await to(
+    HistoryModel.findAll({
+      where, // 使用过滤条件
+      raw: true
+    })
+  );
+
+  if (filteredObj.sortOrder !== '') {
+    if (filteredObj.sortOrder === '升序') {
+      allHistory.sort((a, b) => {
+        const dateA = new Date(a.finishTime);
+        const dateB = new Date(b.finishTime);
+        return dateA - dateB;
+      });
+    } else {
+      allHistory.sort((a, b) => {
+        const dateA = new Date(a.finishTime);
+        const dateB = new Date(b.finishTime);
+        return dateB - dateA;
+      });
+    }
+  }
+
+  if (err) return ctx.err("操作失败", err);
+  ctx.suc("查询成功", allHistory);
+});
+
 module.exports = router.routes() 
